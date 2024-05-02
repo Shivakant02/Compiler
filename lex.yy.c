21eff74c1ce27bf4e936956208a0d3d9c9e55668
Shivakant <kants6397@gmail.com>
2024-05-02 23:19:11 +0530

    Fix token.l and binOD.l files, and add countWord.l and countVowel.l files

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 10
-#define YY_END_OF_BUFFER 11
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,11 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[29] =
+static const flex_int16_t yy_accept[8] =
     {   0,
-        0,    0,   11,   10,    7,   10,    8,    5,    6,    6,
-        6,    6,    6,    9,    8,    6,    6,    1,    6,    6,
-        6,    6,    6,    2,    3,    6,    4,    0
+        0,    0,    4,    3,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -372,17 +370,17 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
-        3,    1,    1,    1,    1,    4,    5,    6,    6,    6,
-        6,    6,    6,    6,    6,    6,    6,    1,    1,    7,
-        1,    8,    1,    1,    9,    9,    9,    9,    9,    9,
-        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
-        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
-        1,    1,    1,    1,    1,    1,   10,   10,   10,   10,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    2,    3,    3,    3,    2,    3,
+        3,    3,    2,    3,    3,    3,    3,    3,    2,    3,
+        3,    3,    3,    3,    2,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    2,    3,    3,    3,
 
-       11,   12,   10,   13,   14,   10,   10,   15,   10,   16,
-       10,   10,   10,   10,   17,   18,   10,   10,   19,   10,
-       10,   10,    1,   20,    1,    1,    1,    1,    1,    1,
+        2,    3,    3,    3,    2,    3,    3,    3,    3,    3,
+        2,    3,    3,    3,    3,    3,    2,    3,    3,    3,
+        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,44 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[21] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1,    1,    1,    1,    2,    1,    1,    1,    2,
-        2,    2,    2,    2,    2,    2,    2,    2,    2,    2
+        1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[30] =
+static const flex_int16_t yy_base[8] =
     {   0,
-        0,    0,   36,   37,   37,   30,   28,   37,    0,   18,
-       20,   18,   17,   37,   23,    0,   11,    0,   16,   12,
-       14,    8,    8,    0,    0,   11,    0,   37,   19
+        0,    0,    4,    5,    5,    5,    5
     } ;
 
-static const flex_int16_t yy_def[30] =
+static const flex_int16_t yy_def[8] =
     {   0,
-       28,    1,   28,   28,   28,   28,   28,   28,   29,   29,
-       29,   29,   29,   28,   28,   29,   29,   29,   29,   29,
-       29,   29,   29,   29,   29,   29,   29,    0,   28
+        7,    1,    7,    7,    7,    7,    0
     } ;
 
-static const flex_int16_t yy_nxt[58] =
+static const flex_int16_t yy_nxt[9] =
     {   0,
-        4,    5,    5,    6,    4,    7,    8,    8,    9,    9,
-       10,    9,    9,   11,    9,    9,    9,   12,   13,    4,
-       16,   27,   26,   25,   24,   23,   22,   21,   15,   20,
-       19,   18,   17,   15,   14,   28,    3,   28,   28,   28,
-       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
-       28,   28,   28,   28,   28,   28,   28
+        4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
-static const flex_int16_t yy_chk[58] =
+static const flex_int16_t yy_chk[9] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-       29,   26,   23,   22,   21,   20,   19,   17,   15,   13,
-       12,   11,   10,    7,    6,    3,   28,   28,   28,   28,
-       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
-       28,   28,   28,   28,   28,   28,   28
+        1,    1,    1,    3,    7,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -453,11 +436,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "token.l"
-#line 2 "token.l"
+#line 1 "countVowel.l"
+#line 2 "countVowel.l"
 #include<stdio.h>
-#line 460 "lex.yy.c"
-#line 461 "lex.yy.c"
+int vowels = 0;
+int consonants = 0;
+#line 445 "lex.yy.c"
+#line 446 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -674,9 +659,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "token.l"
+#line 6 "countVowel.l"
 
-#line 680 "lex.yy.c"
+#line 665 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -703,13 +688,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 29 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 37 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -735,55 +720,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "token.l"
-{printf("IF");}
+#line 7 "countVowel.l"
+{ vowels++; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "token.l"
-{printf("ELSE");}
+#line 8 "countVowel.l"
+{ consonants++; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "token.l"
-{printf("THEN");}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 9 "token.l"
-{printf("THEN");}
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 10 "token.l"
-{printf("RELOP");}
-	YY_BREAK
-case 6:
-YY_RULE_SETUP
-#line 11 "token.l"
-{printf("IDENTIFIER");}
-	YY_BREAK
-case 7:
-YY_RULE_SETUP
-#line 12 "token.l"
-{printf("bracket");}
-	YY_BREAK
-case 8:
-YY_RULE_SETUP
-#line 13 "token.l"
-{printf("NUMBER");}
-	YY_BREAK
-case 9:
-YY_RULE_SETUP
-#line 14 "token.l"
-{exit(0);}
-	YY_BREAK
-case 10:
-YY_RULE_SETUP
-#line 15 "token.l"
+#line 9 "countVowel.l"
 ECHO;
 	YY_BREAK
-#line 787 "lex.yy.c"
+#line 737 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1080,7 +1030,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 29 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1108,11 +1058,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 29 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 28);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1788,7 +1738,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 15 "token.l"
+#line 9 "countVowel.l"
 
 int yywrap()
 {
@@ -1796,11 +1746,8 @@ int yywrap()
 }
 int main()
 {
-    printf("Enter the string\n");
+    printf("Enter a string: ");
     yylex();
+    printf("Vowels: %d\nConsonants: %d\n", vowels, consonants);
     return 0;
-
 }
-
-
-
